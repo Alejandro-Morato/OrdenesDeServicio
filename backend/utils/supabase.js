76a9e5abbc0a37ad4c8dd5271021c7122447fa5e
Alejandro Morato <9abril05@gmail.com>
2026-06-03 19:33:57 -0600

    .

--- backend/utils/supabase.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_KEY; // Service key para backend
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = supabase;
